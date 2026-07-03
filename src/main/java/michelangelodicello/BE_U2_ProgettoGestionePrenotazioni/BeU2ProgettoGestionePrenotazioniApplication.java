976951e4d0a0e9b85aed2ea0cbbb5f1824195723
dicellomichelangelo-debug <dicellomichelangelo@gmail.com>
2026-07-03 09:30:56 +0200

    Initial commit

---
+++ src/main/java/michelangelodicello/BE_U2_ProgettoGestionePrenotazioni/BeU2ProgettoGestionePrenotazioniApplication.java
@@ -0,0 +1,13 @@
+package michelangelodicello.BE_U2_ProgettoGestionePrenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2ProgettoGestionePrenotazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2ProgettoGestionePrenotazioniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/michelangelodicello/BE_U2_ProgettoGestionePrenotazioni/BeU2ProgettoGestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package michelangelodicello.BE_U2_ProgettoGestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2ProgettoGestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
